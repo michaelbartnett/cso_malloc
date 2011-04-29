3895ded21f3c0ae1c92f8ad23ba8081e509b6755
michaelbartnett <michael.bartnett@gmail.com>
2011-04-29 13:31:14 -0400

    Switched to BBEdit. Got mm_init written (still need to test), next step is mm_malloc

--- mm.c
+++ mm.c
@@ -1,13 +1,7 @@
 /*
  * mm-naive.c - The fastest, least memory-efficient malloc package.
  * 
- * In this naive approach, a block is allocated by simply incrementing
- * the brk pointer.  A block is pure payload. There are no headers or
- * footers.  Blocks are never coalesced or reused. Realloc is
- * implemented directly using mm_malloc and mm_free.
- *
- * NOTE TO STUDENTS: Replace this header comment with your own header
- * comment that gives a high level description of your solution.
+ * 
  */
 #include <stdio.h>
 #include <stdlib.h>
@@ -36,31 +30,106 @@ team_t team = {
 };
 
 /* single word (4) or double word (8) alignment */
-#define ALIGNMENT 8
+#define WSIZE 4
+#define DSIZE 8
+#define ALIGNMENT DSIZE
 
 /* rounds up to the nearest multiple of ALIGNMENT */
 #define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
 
-
+/* The size of a size_t type */
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+/* This sets the type CHUNK to be the minimum block size.
+The name CHUNK was chosen because a BLOCK refers to an allocated block of memory,
+and can be any size, and MINIMUM_BLOCK_SIZE is a bit verbose.
+I realize that CHUNKSIZE in the CS:APP implicit free list implementation refers
+to how much memory to start allocating, but I'll likely be increasing the break aligned
+to page size. */
+#if ALIGNMENT == 8
+#define CHUNK unsigned short
+#else
+#define CHUNK unsigned int
+#endif
 
-typedef BOOL int;
-typedef unsigned int void_ptr;
+/* Read and write word value at address 'p' */
+#define GETW(p) (*(unsigned int *)(p))
+#define PUTW(p, val) (*(unsigned int *)(p) = (val))
 
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc) ((size) | (alloc))
+
+/* Read size field */
+#define GET_SIZE(p) (GETW(p) & 0xFFFFFFF8)
+/* Read <allocated?> field */
+#define GET_ALLOC(p) (GETW(p) & 0x1)
+
+/* Get address of header and footer */
+#define GET_BLOCKHDR(bp) ((char *)(bp) - WSIZE)
+#define GET_BLOCKFTR(bp) ((char *)(bp) + GET_SIZE(GET_BLOCKHDR(bp)) - DSIZE)
 
 /*  */
-typedef struct
-{
-	void_ptr address,
-	BOOL free,
-} mm_MemblockHeader_t
+#define GET_NEXTBLOCK(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define GET_PREVBLOCK(bp) ((char *)(bp) - GETSIZE(((char *)(bp) - DSIZE)))
 
-/* 
+/* Using size segregated explicit free lists */
+static CHUNK * free_lists[13] /* Segregate by word size power of 2, up to 4096 words */
+
+static unsigned int PAGE_SIZE;
+static unsigned int PAGE_WORDS;
+
+static unsigned char * heap_start;
+static unsigned char * heap_end;
+
+static int calc_min_bits(size_t size);
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+
+/**
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+	mem_init();
+	
+	memset(free_lists, NULL, sizeof(free_lists));
+	
+	PAGE_SIZE = mem_pagesize();
+	PAGE_WORDS = PAGE_SIZE / ALIGNMENT;
+	
+	/* Initially allocate 1 page of memory plus room for
+		the prologue and epilogue blocks and free block header */
+	if((heap_start = mem_sbrk(PAGE_SIZE + (3 * ALIGNMENT))) == NULL)
+		return -1;
+	
+	heap_end = mem_heap_high();
+	
+	/* Alignment word */
+	PUTW(heap_start, 0);
+	
+	/* Prologue Block header */
+	PUTW(heapstart + (1 * WSIZE), PACK(DISZE, 1));
+	PUTW(heapstart + (2 * WSIZE), PACK(DSIZE, 1));
+	
+	/* Epilogue Block header */
+	PUTW(heap_end - WSIZE + 1, PACK(0, 1));
+	
+	
+	
+	block_addr = heap_start + (2 * ALIGNMENT);
+	num_bytes = PAGE_SIZE;
+	
+	PUTW(GET_BLOCKHDR(chunk_addr), PACK(num_bytes, 0));
+	PUTW(GET_BLOCKFTR(chunk)addr), PACK(num_bytes, 0));
+	/* TODO: Figure out if I need to do the Prologue and Epilogue block.
+	mm_maloc should check the freelists instead of traverse a free list
+	coalesce shouldn't change. Should pull this segment of code out into
+	either mm_free or a mark_free() function or macro.
+	*/
+	/* Ensure first block of free memory is aligned to  */
+	free_lists[calc_min_bits(PAGE_WORDS)] = heap_start + ALIGNMENT;
+	
+	
 	return 0;
 }
 
@@ -87,7 +156,7 @@ void mm_free(void *ptr)
 {
 }
 
-/*
+/**
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
  */
 void *mm_realloc(void *ptr, size_t size)
@@ -108,15 +177,74 @@ void *mm_realloc(void *ptr, size_t size)
 }
 
 
+/**
+ * Calculate bits needed to store a value
+ */
+static int calc_min_bits(size_t size)
+{
+	int bits = 0;
+	while (size >> bits > 1) {
+		bits++;
+	}
+	return bits;
+}
 
 
+/**
+ * Extend the heap by number of words
+ */
+static void *extend_heap(size_t words)
+{
+	char *bp;
+	size_t size;
+	
+	/* Allocate an even number of words to maintain alignment */
+	size = (words %2) ? (words + 1) * WSIZE : words * WSIZE;
+	if ((long)(bp = mem_sbrk(size)) == -1)
+		return NULL;
+	
+	/* Initialize free block header/footer and the epilogue header */
+	PUT(GET_BLOCKHDR(bp), PACK(size, 0));			  /* Free block header */
+	PUT(GET_BLOCKFTR(bp), PACK(size), 0));			  /* Free block header */
+	PUT(GET_BLOCKHDR(GET_NEXTBLOCK(bp)), PACK(0, 1)); /* New epilogue header */
+	
+	/* Coalesce if the previous block was free */
+	return coalesce(bp);
+}
 
-
-
-
-
-
-
-
-
-
+/**
+ * Concatenate adjacent blocks
+ */
+static void *coalesce(void *bp)
+{
+	size_t prev_alloc = GET_ALLOC(GET_BLOCKFTR(GET_PREVBLOCK(bp)));
+	size_t next_alloc = GET_ALLOC(GET_BLOCKHDR(GET_NEXTBLOCK(bp)));
+	size_t size = GET_SIZE(GET_BLOCKHDR(GET_NEXTBLOCK(bp)));
+	
+	if (prev_alloc && !next_alloc) {
+		return bp;
+	}
+	
+	else if (prev_alloc && !next_alloc) {
+		size += GET_SIZE(GET_BLOCKHDR(GET_NEXTBLOCK(bp)));
+		PUTW(GET_BLOCKHDR(bp), PACK(size, 0));
+		PUT(GET_BLOCKFTR(bp), PACK(size, 0));
+	}
+	
+	else if (!prev_alloc && next_alloc) {
+		size += GET_SIZE(GET_BLOCKHDR(GET_PREVBLOCK(bp)));
+		PUTW(GET_BLOCKFTR(bp), PACK(size, 0));
+		PUTW(GET_BLOCKHDR(GET_PREVBLOCK(b)), PACK(size, 0));
+		bp = GET_PREVBLOCK(bp);
+	}
+	
+	else {
+		size += GET_SIZE(GET_BLOCKHDR(GET_PREVBLOCK(bp))) + 
+		  GET_SIZE(GET_BLOCKFTR(GET_NEXTBLOCK(bp)));
+		PUTW(GET_BLOCKHDR(GET_PREVBLOCK(b)), PACK(size, 0));
+		PUTW(GET_BLOCKFTR(GET_NEXTBLOCK(bp)), PACK(size, 0));
+		bp = GET_PREVBLOCK(bp);
+	}
+	
+	return bp;
+}
